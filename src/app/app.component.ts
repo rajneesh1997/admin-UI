2a60fc4abf95e4c93bdf9342dd7110a4a9f63d89
Rajneesh Singh <jsr.raj2011@gmail.com>
2021-09-24 21:27:59 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,132 @@
-import { Component } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { HttpClient } from '@angular/common/http';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
+export interface PeriodicElement {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+  edit: boolean;
+  position: number;
+}
+
+/**
+ * @title Table with selection
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'admin-UI';
+export class AppComponent implements OnInit {
+  checked: boolean;
+  indeterminate: boolean;
+  pageSize = 10;
+  displayedColumns: string[] = ['select', 'name', 'email', 'role', 'actions'];
+
+  pageEvent: PageEvent;
+
+  fetchApi =
+    'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json';
+
+  dataSource: MatTableDataSource<PeriodicElement>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  selection: SelectionModel<PeriodicElement>;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.checked = false;
+    this.indeterminate = false;
+    this.httpClient.get(this.fetchApi).subscribe((data: any) => {
+      data.forEach((element, index) => {
+        element.edit = false;
+        element.position = index + 1;
+      });
+      const ELEMENT_DATA: PeriodicElement[] = data;
+      this.dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+      this.selection = new SelectionModel<PeriodicElement>(true, []);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection?.selected?.length;
+    const page = this.dataSource?.paginator?.pageSize;
+    return numSelected === page;
+  }
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ? this.selection.clear() : this.selectRows();
+  }
+
+  selectRows() {
+    let i =
+      this.dataSource.paginator.pageIndex * this.dataSource.paginator.pageSize;
+    let end =
+      (this.dataSource.paginator.pageIndex + 1) *
+      this.dataSource.paginator.pageSize;
+    for (i; i < end; i++) {
+      this.selection.select(this.dataSource.data[i]);
+    }
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+      row.position + 1
+    }`;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteRow(index: number) {
+    this.dataSource.data.splice(index, 1);
+    this.dataSource = new MatTableDataSource<PeriodicElement>(
+      this.dataSource.data
+    );
+    this.selection = new SelectionModel<PeriodicElement>(true, []);
+    setTimeout(() => (this.dataSource.paginator = this.paginator), 0);
+  }
+
+  editRow(index: number) {
+    this.dataSource.data[index].edit = true;
+  }
+
+  saveRow(index: number) {
+    this.dataSource.data[index].edit = false;
+  }
+
+  editRowValue(index, key, event) {
+    if (key == 'name') this.dataSource.data[index].name = event.target.value;
+    else {
+      if (key == 'email')
+        this.dataSource.data[index].email = event.target.value;
+      else {
+        if (key == 'role')
+          this.dataSource.data[index].role = event.target.value;
+      }
+    }
+  }
+
+  deleteSelected() {
+    this.dataSource.data = this.dataSource.data.filter(
+      (el) => !this.selection?.selected.includes(el)
+    );
+    this.selection = new SelectionModel<PeriodicElement>(true, []);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from "@angular/material/input";
+import { HttpClientModule } from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
